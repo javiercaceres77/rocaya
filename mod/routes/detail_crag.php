e811e52f4884ccc8b29bb203df23899e2153d9e4
Javier Caceres <javier.caceres77@gmail.com>
2015-03-10 11:58:01 +0000

    Update detail_crag.php
    removed comments, stats, etc

--- mod/routes/detail_crag.php
+++ mod/routes/detail_crag.php
@@ -46,64 +46,8 @@ while($record = my_fetch_array($select_sectors)) {
 ?>
         </ul>
       </div>
-      <!-- comentarios de la escuela aqui --></td>
-    <td valign="top" class="default_text"><?php 
-	// if user is logged in
-if($_SESSION['Login']['UserID'] != $conf_generic_user_id) {
-	# get climbs for this user in this crag
-	$sql = 'SELECT r.route_id, r.sector_id, r.rname, r.grade, s.sname, s.sector_id, ur.climb_type, ur.climb_date, s.sector_id_url
-FROM routes r
-INNER JOIN users_routes ur ON ur.route_id = r.route_id
-INNER JOIN sectors s ON s.sector_id = r.sector_id
-WHERE ur.user_id = '. $_SESSION['Login']['UserID'] .' AND r.crag_id = \''. $_GET['detail'] .'\'
-ORDER BY ur.climb_date DESC';
-
-	$select_user_routes = my_query($sql, $conex);
-	
-	$num_user_routes = my_num_rows($select_user_routes);
-?>
-      <div class="standard_container"> <span class="standard_cont_title">Estadísticas</span><br>
-        <?php
-	if($num_user_routes > 0) {
-		$arr_climb_types = dump_table('climbs_types', 'climb_type_id', 'desc_es');
-?>
-        En esta escuela has hecho <?php echo $num_user_routes; ?> vías;<br>
-        Tus últimas ascensiones son:
-        <ul class="standard_bullet_list">
-          <?php
-		$counter = 20;
-		while($record = my_fetch_array($select_user_routes)) {
-			$counter--;
-			echo '<li>'. date2lan($record['climb_date'], 'med') .', '. $record['rname'] .' ('. $record['grade'] .'), '. $arr_climb_types[$record['climb_type']] .' &ndash; ';
-			echo 'Sector <a href="'. $conf_main_page .'?mod='. $_GET['mod'] .'&view=detail_sector&detail='. $record['sector_id'] .'&id='. $record['sector_id_url'] .'">'. $record['sname'] .'</a></li>';
-			if($counter == 0) break;
-		}
-?>
-        </ul>
-        <?php
-	}	//	if($num_user_routes > 0) {
-	else
-		echo 'No has hecho ninguna ascensión en esta escuela todavía.';
-?>
-      </div>
-      <?php
-	if($_SESSION['Login']['modules']['stats']) {	# The stats module is available
-		echo '<br><br><a href="'. $conf_main_page .'?mod=stats&view=user_stats">Ver estadísticas completas</a>';
-	}
-?>
-      <?php 
-} //	if($_SESSION['Login']['UserID'] != $conf_generic_user_id) {
-?>
-      <div class="standard_container"> <span class="standard_cont_title">Comentarios de la escuela</span><br>
-<?php
-$crag_obj = new crag($_GET['detail']);
-$crag_obj->print_crag_comments();
-
-if($_SESSION['Login']['UserID'] != $conf_generic_user_id)
-	$crag_obj->print_crag_comment_box();
-else
-	echo '<a href="'. $conf_main_page .'?mod=home&view=new_user">Regístrate</a> para escribir comentarios<br />';
-?>
-</div></td>
+    </td>
+    <td valign="top" class="default_text">
+    </td>
   </tr>
 </table>
